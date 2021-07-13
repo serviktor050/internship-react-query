68b63155f75f8b0da780dec4e5a8a1d49d7592a1
serviktor050 <mr.s-gagulin@yandex.ru>
2021-07-13 13:02:19 +0300

    Скорректировано расположение контекста для калькулятора

--- src/App.js
+++ src/App.js
@@ -13,36 +13,27 @@ import "antd/dist/antd.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { LoginRegisterProvider } from "./components/Pages/LoginRegister/LoginRegisterContext";
-import { MortgageCalculatorMenuProvider } from "./components/Pages/MortgageCalculator/MortgageCalculatorContexts/MortgageCalculatorContextMenu";
-import { MortgageCalculatorFormProvider } from "./components/Pages/MortgageCalculator/MortgageCalculatorContexts/MortgageCalculatorContextForm";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <LoginRegisterProvider>
-      <MortgageCalculatorMenuProvider>
-        <MortgageCalculatorFormProvider>
-          <QueryClientProvider client={queryClient}>
-            <Router>
-              <Menu />
-              <Switch>
-                <Route path="/timer" component={Timer} />
-                <Route
-                  path="/mortgage-calculator"
-                  component={MortgageCalculator}
-                />
-                <Route path="/list-users/user-:id" component={User} />
-                <Route path="/list-users" component={ListUsers} />
-                <Route path="/register" component={Register} />
-                <Route path="/login" component={Login} />
-                <Route exact path="/" component={Home} />
-              </Switch>
-            </Router>
-            <ReactQueryDevtools initialIsOpen={false} />
-          </QueryClientProvider>
-        </MortgageCalculatorFormProvider>
-      </MortgageCalculatorMenuProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Menu />
+          <Switch>
+            <Route path="/timer" component={Timer} />
+            <Route path="/mortgage-calculator" component={MortgageCalculator} />
+            <Route path="/list-users/user-:id" component={User} />
+            <Route path="/list-users" component={ListUsers} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </LoginRegisterProvider>
   );
 }
--- src/components/Pages/MortgageCalculator/MortgageCalculator.jsx
+++ src/components/Pages/MortgageCalculator/MortgageCalculator.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { useLoginRegister } from "../LoginRegister/LoginRegisterContext";
 import { Redirect } from "react-router-dom";
 import Calculator from "../MortgageCalculator/Calculator";
+import { MortgageCalculatorMenuProvider } from "../MortgageCalculator/MortgageCalculatorContexts/MortgageCalculatorContextMenu";
+import { MortgageCalculatorFormProvider } from "../MortgageCalculator/MortgageCalculatorContexts/MortgageCalculatorContextForm";
 
 export default function MortgageCalculator() {
   const { userToken } = useLoginRegister();
   return (
     <>
       {userToken === null && <Redirect to="/" />}
-      <h1>Ипотечный калькулятор</h1>
-      <p>С возможностью расчета досрочного погашения</p>
-      <Calculator />
+      <MortgageCalculatorMenuProvider>
+        <MortgageCalculatorFormProvider>
+          <h1>Ипотечный калькулятор</h1>
+          <p>С возможностью расчета досрочного погашения</p>
+          <Calculator />
+        </MortgageCalculatorFormProvider>
+      </MortgageCalculatorMenuProvider>
     </>
   );
 }
